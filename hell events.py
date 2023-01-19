99e00de7f1f97e1a1f1c86f8954f8de7d1fcf132
MIKIBURGOS <57463120+MIKIBURGOS@users.noreply.github.com>
2023-01-19 21:05:48 +0100

    Update hell events.py

--- hell events.py
+++ hell events.py
@@ -15,7 +15,7 @@ for construction in constructions:
 
 i_index = 0
 
-for i in time_table:
+for i in time_table:  # Translate every item in time_table to seconds
     x_index = 0
     for x in i:
         time_table[i_index][x_index] = translate_time(time_table[i_index][x_index])
@@ -28,8 +28,32 @@ i_index = 0
 for i in might_table:
     x_index = 0
     for x in i:
-        might_conversor[f'{constructions[i_index]}_{x_index + 1}'] = might_table[i_index][x_index]
-        time_conversor[f'{constructions[i_index]}_{x_index + 1}'] = time_table[i_index][x_index]
+        might_dict[f'{constructions[i_index]}_{x_index + 1}'] = might_table[i_index][x_index]
+        time_dict[f'{constructions[i_index]}_{x_index + 1}'] = time_table[i_index][x_index]
+        food_dict[f'{constructions[i_index]}_{x_index + 1}'] = food_table[i_index][x_index]
+        stone_dict[f'{constructions[i_index]}_{x_index + 1}'] = stone_table[i_index][x_index]
+        timber_dict[f'{constructions[i_index]}_{x_index + 1}'] = timber_table[i_index][x_index]
+        ore_dict[f'{constructions[i_index]}_{x_index + 1}'] = ore_table[i_index][x_index]
+        rss_dict[f'{constructions[i_index]}_{x_index + 1}'] = 0
+        try:  # Some values aren't numbers (N/A, None, etc), so it just sums up as many as it can for every construction
+            rss_dict[f'{constructions[i_index]}_{x_index + 1}'] += food_table[i_index][x_index]
+        except TypeError:
+            pass
+        try:
+            rss_dict[f'{constructions[i_index]}_{x_index + 1}'] += stone_table[i_index][x_index]
+        except TypeError:
+            pass
+        try:
+            rss_dict[f'{constructions[i_index]}_{x_index + 1}'] += timber_table[i_index][x_index]
+        except TypeError:
+            pass
+        try:
+            rss_dict[f'{constructions[i_index]}_{x_index + 1}'] += ore_table[i_index][x_index]
+        except TypeError:
+            pass
+
+
+
         try:  # Some values are "", None, N/A, etc (for example, castle is already lv1 when you start the game)
             ratio_conversor[f'{constructions[i_index]}_{x_index + 1}'] = int(might_table[i_index][x_index]) / int(translate_time(time_table[i_index][x_index]))
         except (ValueError, AttributeError, ZeroDivisionError, TypeError):
@@ -41,3 +65,4 @@ add_gear(construction_gear)  # Take into account your gear (for more info, check
 add_helps()  # Take into account the guildmates' help (for more info, check the function in functions.py)
 add_vip(13)  # Take into account your VIP level (for more info, check the function in functions.py)
 
+print(rss_dict)
